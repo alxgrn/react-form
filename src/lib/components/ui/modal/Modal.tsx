739b08d19844c4937ae6bdd51c098c33b78a0c43
alxgrn <alxgrn@users.noreply.github.com>
2024-07-23 16:39:31 +0300

    Исправлен баг с фиксацией скролла документа при открытом модальном окне. Обработка перенесена в Portal.

--- src/lib/components/ui/modal/Modal.tsx
+++ src/lib/components/ui/modal/Modal.tsx
@@ -1,53 +1,18 @@
-import React, { FC, PropsWithChildren, useEffect, useRef, useState } from 'react';
+import React, { FC, PropsWithChildren, useEffect, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import Portal from '../portal/Portal';
 import './Modal.css';
 
 const TIMEOUT = 100; // Должен согласовываться с длительностью анимации появления в .css
-const MODAL_CLASS_NAME = 'alxgrn-modal-open';
 
 export type ModalProps = {
     isOpen: boolean;
     onClose: () => void;
-    close?: boolean;
+    close?: boolean; // рисовать или нет кнопку закрытия
 };
 
 const Modal: FC<PropsWithChildren<ModalProps>> = ({ children, isOpen, onClose, close = true }) => {
     const nodeRef = useRef(null);
-    const [ isFirstOpener, setIsFirstOpener ] = useState(false);
-    
-    // При открытии окна будем запрещать прокрутку страницы
-    useEffect(() => {
-        const clear = () => {
-            // Если не мы устанавливали значения, не нам их и убирать
-            if (!isFirstOpener) return;
-            const scrollY = document.body.style.top;
-            document.body.style.position = '';
-            document.body.style.top = '';
-            document.body.style.left = '';
-            document.body.style.right = '';
-            window.scrollTo(0, parseInt(scrollY || '0') * -1);
-            document.body.classList.remove(MODAL_CLASS_NAME);
-            setIsFirstOpener(false);
-        };
-
-        if (isOpen) {
-            // Если уже открыто какое-то модальное окно, ничего не делаем
-            if (document.body.classList.contains(MODAL_CLASS_NAME)) return;
-            const scrollY = `-${window.scrollY}px`;
-            document.body.style.position = 'fixed';
-            document.body.style.top = scrollY;
-            document.body.style.left = '0px';
-            document.body.style.right = '0px';
-            document.body.classList.add(MODAL_CLASS_NAME);
-            setIsFirstOpener(true); // Отметим что мы первые, кто открыл модалку
-        } else {
-            clear();
-        }
-
-        // Очищаем все при размонтировании элемента т.к. это аналог его закрытия
-        return () => clear();
-    }, [ isOpen, isFirstOpener ]);
 
     // Отслеживаем нажатие ESC для закрытия окна
     useEffect(() => {
@@ -62,6 +27,7 @@ const Modal: FC<PropsWithChildren<ModalProps>> = ({ children, isOpen, onClose, c
         };
     }, [ isOpen, onClose ]);
 
+    // Обработчик клика на кнопку закрытия окна или на область вокруг окна
     const onClick = (e: React.MouseEvent) => {
         // Без этого возможно двойное срабатывание при одном клике
         e.stopPropagation();
@@ -72,7 +38,7 @@ const Modal: FC<PropsWithChildren<ModalProps>> = ({ children, isOpen, onClose, c
     // if (!isOpen) return null;
 
 	return (
-        <Portal id='alxgrn-modal'>
+        <Portal id='alxgrn-modal' fixBody>
             <CSSTransition
                 in={isOpen}
                 timeout={TIMEOUT}
--- src/lib/components/ui/portal/Portal.tsx
+++ src/lib/components/ui/portal/Portal.tsx
@@ -3,16 +3,55 @@ import { createPortal } from 'react-dom';
 
 export type PortalProps = {
     id: string;
+    fixBody?: boolean; // Флаг того, что надо запрещать скроллить основное окно когда портал непустой. Нужно для Modal
 };
 
-const Portal: FC<PropsWithChildren<PortalProps>> = ({ id, children }) => {
+const Portal: FC<PropsWithChildren<PortalProps>> = ({ id, fixBody, children }) => {
     let portalElement = document.getElementById(id);
     if(!portalElement) {
         portalElement = document.createElement('div');
         portalElement.setAttribute('id', id);
         document.body.append(portalElement);
+        // Заведем наблюдателя за содержимым если нужно фиксировать прокрутку страницы
+        if (fixBody) {
+            let observer = new MutationObserver(() => {
+                var isEmpty = portalElement?.innerHTML === '';
+                // console.log(isEmpty ? 'Нет модалки' : 'Есть модалка');
+                if (isEmpty) releaseBody(); else freezeBody();
+            });
+            observer.observe(portalElement, {
+                childList: true, // наблюдать за непосредственными детьми
+            });
+        }
     }
     return createPortal(children, portalElement);
+};
+
+// При открытии модального окна будем запрещать прокрутку страницы
+// https://habr.com/ru/articles/459876/
+// https://css-tricks.com/prevent-page-scrolling-when-a-modal-is-open/
+
+const freezeBody = () => {
+    // console.log(`Portal -> freezeBody`);
+    const scrollY = `-${window.scrollY}px`;
+    document.body.style.position = 'fixed';
+    document.body.style.top = scrollY;
+    document.body.style.left = '0px';
+    document.body.style.right = '0px';
+    // document.body.classList.add(MODAL_CLASS_NAME);
+};
+
+// TODO: При возврате прокрутки происходит скролл вниз на несколько пикселов.
+//       Пока не понятно почему и что с этим делать.
+const releaseBody = () => {
+    // console.log(`Portal -> releaseBody`);
+    const scrollY = document.body.style.top;
+    document.body.style.position = '';
+    document.body.style.top = '';
+    document.body.style.left = '';
+    document.body.style.right = '';
+    window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    // document.body.classList.remove(MODAL_CLASS_NAME);
 };
 
 export default Portal;
